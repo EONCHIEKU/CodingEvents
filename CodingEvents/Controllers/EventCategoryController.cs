f9c82d50bdb6d9d0edfb1bcb92296d2e2e69e0e8
Esther Onchieku <estheronchieku@gmail.com>
2023-04-06 15:39:24 -0500

    initial commit for AddEventCategoryViewModel Studio

--- CodingEvents/Controllers/EventCategoryController.cs
+++ CodingEvents/Controllers/EventCategoryController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using CodingEvents.Data;
 using CodingEvents.Models;
+using CodingEvents.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -26,6 +27,30 @@ namespace CodingEvents.Controllers
             return View(categories);
 
         }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel addEventCategoryViewModel = new AddEventCategoryViewModel();
+            return View(addEventCategoryViewModel);
+        }
+        [HttpPost("/Create")]
+        public IActionResult ProcessCreateEventCategoryForm(AddEventCategoryViewModel addEventCategoryViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory category = new EventCategory
+                {
+                    Name = addEventCategoryViewModel.Name
+                    
+                };
+                context.Categories.Add(category);
+                context.SaveChanges();
+                return Redirect("/EventCategory");
+            }
+            return View("Create", addEventCategoryViewModel);
+
+        }
     }
 }
+
 
---
+++ CodingEvents/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CodingEvents.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage = "Please enter a category.")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Name must contain a minimum of 3 characters and a maximum of 20.")]
+        public string? Name { get; set; }
+    }
+}
